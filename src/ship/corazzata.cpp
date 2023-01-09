4b8640c57311ef8cb9696297caa21ed7d520d284
Mattichs <mattiabastia@gmail.com>
2023-01-09 11:39:24 +0100

    .

--- src/ship/corazzata.cpp
+++ src/ship/corazzata.cpp
@@ -8,10 +8,3 @@ corazzata::corazzata(asset way, coords& center) : ship(way, center){
 	// controllo che le posizioni non escano dalla griglia
 	std::vector<coords> positions = get_position(center, length, way);
 }
-
-corazzata::corazzata(asset way, coords& center) : ship(way, center){
-	alias = 'C';
-	hp = {1, 1, 1, 1, 1};
-	length = 5;
-	std::vector<coords> positions = get_position(center, length, way);
-}
