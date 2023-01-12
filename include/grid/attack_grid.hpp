f64830f4b00d32d71076afaf86137951c218ef88
CognoXD <cognac14265@gmail.com>
2023-01-12 11:19:47 +0100

    aggiunto enemy_ships

--- include/grid/attack_grid.hpp
+++ include/grid/attack_grid.hpp
@@ -11,10 +11,13 @@
 class attack_grid : public grid {
     private:
         char matrix[12][12];
+        std::vector<coords> enemy_positions;
         defense_grid& dgrid; 
     public:
         attack_grid(defense_grid& dg);
         bool fire(coords c);
+        void enemy_ships(coords& c);
+        void reset_enemy_pos();
         friend std::ostream& operator <<( std::ostream& os,const attack_grid&);
 
 };
--- src/grid/attack_grid.cpp
+++ src/grid/attack_grid.cpp
@@ -8,7 +8,7 @@
 attack_grid::attack_grid( defense_grid& dg): dgrid(dg){
         for(int i=0;i<12;i++){
         for(int j=0;j<12;j++)
-            matrix[i][j]='#';
+            matrix[i][j]='~';
     }
 }//fine costruttore attack_grid
 
@@ -20,6 +20,39 @@ bool attack_grid::fire(coords c){
     matrix[c.first][c.second]='O';
     return false;
 }
+
+void attack_grid::enemy_ships(coords& c){
+    std::vector<coords> enemy_positions;
+
+    if(!valid_box(c)) throw std::invalid_argument("");
+
+    else{
+        for(int i = 0; i < 3; i++){ //righe
+            for(int j = 0; j < 3; j++){ //colonne
+                coords box = {i,j};
+                if(valid_box(box)){
+                    if(dgrid.is_ship(box)){
+                        bool res=false;
+                        for(coords el:enemy_positions)
+                                res=((box.first==el.first&&box.second==el.second)||matrix[box.first][box.second]=='X');
+                        if(!res)
+                            enemy_positions.push_back(box);
+                    }
+                }
+            }
+        }
+        for(coords el:enemy_positions){
+            if(matrix[el.first][el.second]!='X')
+                matrix[el.first][el.second]='~';
+        }
+    }
+ }
+
+ void attack_grid::reset_enemy_pos(){
+     for(coords el:enemy_positions){
+            matrix[el.first][el.second]='Y';
+        }
+ }
 
 std::ostream& operator <<(std::ostream& os,const attack_grid& ag){
     for(int i=0;i<12;i++){
