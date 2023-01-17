79682cb6df5c38224545e817c54bdc9ca55d2c3c
Polilavergineumile <polichristian21@gmail.com>
2023-01-17 12:31:37 +0100

    Rimossa coords_trans da utility

--- src/utility.cpp
+++ src/utility.cpp
@@ -54,43 +54,6 @@ std::string two_coords_to_string(coords& c1, coords& c2) {
     return s;
 }
 
-// change coords char,int ---> int,int 
-std::vector<coords> coords_translation(std::string s) {
-    coords c1 {0,0};
-    coords c2 {0,0};
-    // prendo la stringa la divido in due e procedo
-    std::string s1 = s.substr(0, s.find(" "));
-    std::string s2 = s.substr(s.find(" ") + 1);
-    if(!(isalpha(s1[0]) && isalpha(s2[0]))) throw std::invalid_argument("Inserisci le coordinate in modo corretto! (il primo carattere deve essere una lettera)\n");
-    if(s1.length() == 3) {
-        // prima coordinata
-        if((short) s1[0] > 73)	c1.first = (short) s1[0] - 67;
-	    else c1.first = (short) s1[0] - 65;
-        c1.second = stoi(s1.substr(1,2)) - 1;
-    } else {
-        if((short) s1[0] > 73)	c1.first = (short) s1[0] - 67;
-	    else c1.first = (short) s1[0] - 65;
-        c1.second = s[1] - '0' - 1;
-    }
-    if(s2.length() == 3) {
-        // seconda coordinata
-        if((short) s2[0] > 73)	c2.first = (short) s2[0] - 67;
-	    else c2.first = (short) s2[0] - 65;
-        c2.second = stoi(s2.substr(1,2)) -1;
-    } else if(s2.length() == 2){
-        if((short) s2[0] > 73)	c2.first = (short) s2[0] - 67;
-	    else c2.first = (short) s2[0] - 65;
-        c2.second = s2[1] - '0' -1; // es. conversione '0' -> 0
-    } else {
-        throw std::invalid_argument("Inserisci le coordinate in modo corretto \n");
-    }
-    std::vector<coords> v;
-    v.push_back(c1);
-    v.push_back(c2);
-    return v;
-}
-
-
 coords get_center(std::vector<coords> v) {
     // se i primi due sono uguali è orizzontale
     if(v[0].first == v[1].first) return coords(v[0].first, (v[0].second + v[1].second)/2);
